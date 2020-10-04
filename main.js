db189896986a4e9d24e08e1483220ec509e20dc9
nour mouazen <nh.yu.mo@hotmail.com>
2020-10-04 14:55:20 +0300

    1

--- main.js
+++ main.js
@@ -1,15 +1,12 @@
 $("#save").click(function() {
-	var bookname=$("#book-name").val();
+   var bookname=$("#book-name").val();
 	var auther=$("#auther-name").val();
 	var bookcover=$("#book-cover").val();
 	var quote=$("#quote").val();
 	var rate =$("#rate").val();
-	var ownerwindow = window.open()
-	$("<li>"+ownerwindow.document.write(bookname),
-ownerwindow.document.write(auther),
-ownerwindow.document.write(bookcover),
-ownerwindow.document.write(quote),
-ownerwindow.document.write(rate)+"</li>").appendTo("#list")
+	var url = "file:///C:/Users/Toshiba/Desktop/project-1/project-1/two.html"
+	window.open(url)
+ownerwindow.document.write(bookname)
 
 
 });
